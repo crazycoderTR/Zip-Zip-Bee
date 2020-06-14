94e3373bc8c9522223a8d34d34d5c21eee238717
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2020-06-14 14:04:59 +0300

    first commit

---
+++ android/src/com/crazycoder/io/bee/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.crazycoder.io.bee;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.crazycoder.io.bee.ZipZipBee;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ZipZipBee(), config);
+	}
+}
