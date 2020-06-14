178e387fa14f839025d7e398d5e3ee5a5d97cfd3
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2020-06-14 15:49:41 +0300

    background has added

--- core/src/com/crazycoder/io/bee/ZipZipBee.java
+++ core/src/com/crazycoder/io/bee/ZipZipBee.java
@@ -7,27 +7,28 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class ZipZipBee extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+
+	SpriteBatch batch; // cizim icin kullanacagim arac
+	Texture background;
 	
 	@Override
 	public void create () {
+		// oncreate metodu ile ayni
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		background = new Texture("background.png");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		// oyunun render edildigi kisim, oyun alani
+		batch.begin(); // begin ve end arasina oyunu cizecegiz, neler olacagini
+
+		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+		batch.end(); // begin ve end arasina oyunu cizecegiz, neler olacagini
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
 	}
 }
